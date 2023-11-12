d43c5b4c6332e48b9ea37fd4fe94ba9fc23a61d7
Rakesh Saha <87816775+rakesh-saha@users.noreply.github.com>
2023-11-12 19:49:26 +0530

    Update math.js

--- math.js
+++ math.js
@@ -3,14 +3,15 @@ window.onload = function () {
         var a = parseFloat(document.getElementById("a1").value);
         var b = parseFloat(document.getElementById("b1").value);
         var c = parseFloat(document.getElementById("c1").value);
-        var d = b*b - 4*a*c;
+        var d = (b*b - 4*a*c);
         var res = (-b)/(2*a);
 
         if(d==0){
             document.getElementById("result1").value=res.toFixed(4);
         }
         else if(d<0){
-            document.getElementById("result1").value="("+res.toFixed(4)+" + "+(-d/(2*a)).toFixed(4)+" i ) & ( "+res.toFixed(4)+" - "+(-d/(2*a)).toFixed(4)+" i)";
+            d=Math.sqrt(-d)/(2*a);
+            document.getElementById("result1").value="("+res.toFixed(4)+" + "+(d).toFixed(4)+" i ) & ( "+res.toFixed(4)+" - "+(d).toFixed(4)+" i)";
         }else{
             var x = parseFloat(res)+parseFloat((Math.sqrt(d))/(2*a));
             var y = parseFloat(res)-parseFloat((Math.sqrt(d))/(2*a));
