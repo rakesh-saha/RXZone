9af6888f24ec2c332390e1c0546b49a8c7010040
Rakesh Saha <87816775+rakesh-saha@users.noreply.github.com>
2023-11-12 19:18:57 +0530

    Update math.js

--- math.js
+++ math.js
@@ -4,20 +4,18 @@ window.onload = function () {
         var b = parseFloat(document.getElementById("b1").value);
         var c = parseFloat(document.getElementById("c1").value);
         var d = b*b - 4*a*c;
-        var res = (-b)/(2*a).toFixed(4);
-        res=res.toFixed(4);
+        var res = (-b)/(2*a);
 
         if(d==0){
-            document.getElementById("result1").value=res;
+            document.getElementById("result1").value=res.toFixed(4);
         }
         else if(d<0){
-            document.getElementById("result1").value="("+res+" + "+(-d/(2*a)).toFixed(4)+" i ),( "+res+" - "+(-d/(2*a)).toFixed(4)+" i)";
+            document.getElementById("result1").value="("+res.toFixed(4)+" + "+(-d/(2*a)).toFixed(4)+" i ) & ( "+res.toFixed(4)+" - "+(-d/(2*a)).toFixed(4)+" i)";
         }else{
-            var im = (Math.sqrt(d))/(2*a);
-            im=im.toFixed(4);
+            var x = parseFloat(res)+parseFloat((Math.sqrt(d))/(2*a));
+            var y = parseFloat(res)-parseFloat((Math.sqrt(d))/(2*a));
 
-            document.getElementById("result1").value=(res+im)+" , "+(res-im);
+            document.getElementById("result1").value=(x).toFixed(4)+" & "+(y).toFixed(4);
         }
-        //console.log("Hello world!");
     }
 }
